4c136d49e74cd1ddd31cc89ff9048d6532df0915
Debabrata Poddar <debobratapoddar3@gmail.com>
2019-06-02 11:03:05 +0530

    MongoDB Server From Lab Has been Setup

--- server.js
+++ server.js
@@ -27,12 +27,25 @@ app.use(stylus.middleware(
 
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/techMania', {useNewUrlParser: true});
+if(env === "development"){
+    mongoose.connect('mongodb://localhost/techMania', {useNewUrlParser: true});
+}else{
+    mongoose.connect('mongodb+srv://debobratap:Debukgec1991@cluster0-bjwzh.mongodb.net/TechMania?retryWrites=true', {useNewUrlParser: true});
+};
+
 db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "connection error..."));
 db.once('open', function callback() {
-    console.log("techMania db opened!")
+    console.log("TechMania db opened!")
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc.message;
 });
 
 app.get('/partials/:partialPath', function(req, res){
@@ -41,9 +54,9 @@ app.get('/partials/:partialPath', function(req, res){
 
 
 app.get('*', function(req, res){
-    res.render('index', {user: "Debobrata",title:"Homepage"});
+    res.render('index', {mongoMessage});
 });
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Lisenting on port ' + port + '...');
